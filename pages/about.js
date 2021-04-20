2146b6ac1b8c45a428dd7ec1e424841b9d7a6c51
Umut Akin <umutakin@fastmail.com>
2021-04-20 21:46:52 +0300

    nextjs 09 - images & metadata

--- pages/about.js
+++ pages/about.js
@@ -1,26 +1,37 @@
+import Head from 'next/head';
+
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae
-                excepturi facere recusandae ipsum est exercitationem aperiam
-                voluptatibus porro veritatis qui delectus, minus quidem soluta
-                voluptatem asperiores ipsam nobis commodi laudantium.
-            </p>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae
-                excepturi facere recusandae ipsum est exercitationem aperiam
-                voluptatibus porro veritatis qui delectus, minus quidem soluta
-                voluptatem asperiores ipsam nobis commodi laudantium.
-            </p>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae
-                excepturi facere recusandae ipsum est exercitationem aperiam
-                voluptatibus porro veritatis qui delectus, minus quidem soluta
-                voluptatem asperiores ipsam nobis commodi laudantium.
-            </p>
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninja" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Beatae excepturi facere recusandae ipsum est exercitationem
+                    aperiam voluptatibus porro veritatis qui delectus, minus
+                    quidem soluta voluptatem asperiores ipsam nobis commodi
+                    laudantium.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Beatae excepturi facere recusandae ipsum est exercitationem
+                    aperiam voluptatibus porro veritatis qui delectus, minus
+                    quidem soluta voluptatem asperiores ipsam nobis commodi
+                    laudantium.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Beatae excepturi facere recusandae ipsum est exercitationem
+                    aperiam voluptatibus porro veritatis qui delectus, minus
+                    quidem soluta voluptatem asperiores ipsam nobis commodi
+                    laudantium.
+                </p>
+            </div>
+        </>
     );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,32 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
     return (
-        <div>
-            <h1 className={ styles.title }>Home Page</h1>
-            <p className={ styles.text }>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-                esse consequatur saepe molestias provident, itaque accusantium
-                tempore illo mollitia non pariatur expedita est ducimus omnis
-                reprehenderit, doloribus, aliquam assumenda ut?
-            </p>
-            <p className={ styles.text }>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-                esse consequatur saepe molestias provident, itaque accusantium
-                tempore illo mollitia non pariatur expedita est ducimus omnis
-                reprehenderit, doloribus, aliquam assumenda ut?
-            </p>
-            <Link href="/ninjas">
-                <a className={ styles.btn}>See Ninja Listing</a>
-            </Link>s
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | Home</title>
+                <meta name="keywords" content="ninja"/>
+            </Head>
+            <div>
+                <h1 className={styles.title}>Home Page</h1>
+                <p className={styles.text}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Porro esse consequatur saepe molestias provident, itaque
+                    accusantium tempore illo mollitia non pariatur expedita est
+                    ducimus omnis reprehenderit, doloribus, aliquam assumenda
+                    ut?
+                </p>
+                <p className={styles.text}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Porro esse consequatur saepe molestias provident, itaque
+                    accusantium tempore illo mollitia non pariatur expedita est
+                    ducimus omnis reprehenderit, doloribus, aliquam assumenda
+                    ut?
+                </p>
+                <Link href="/ninjas">
+                    <a className={styles.btn}>See Ninja Listing</a>
+                </Link>
+                s
+            </div>
+        </>
     );
 }
