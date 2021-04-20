54aa610edff4c61c44dce9fec1a43aa9687cbd98
Umut Akin <umutakin@fastmail.com>
2021-04-20 20:43:03 +0300

    nextjs 03 - adding other components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import Head from "next/head";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-    return <div>
-        <h1>Home Page</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro esse consequatur saepe molestias provident, itaque accusantium tempore illo mollitia non pariatur expedita est ducimus omnis reprehenderit, doloribus, aliquam assumenda ut?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro esse consequatur saepe molestias provident, itaque accusantium tempore illo mollitia non pariatur expedita est ducimus omnis reprehenderit, doloribus, aliquam assumenda ut?</p>
-    </div>
+    return (
+        <div>
+            <Navbar />
+            <h1>Home Page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+                esse consequatur saepe molestias provident, itaque accusantium
+                tempore illo mollitia non pariatur expedita est ducimus omnis
+                reprehenderit, doloribus, aliquam assumenda ut?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+                esse consequatur saepe molestias provident, itaque accusantium
+                tempore illo mollitia non pariatur expedita est ducimus omnis
+                reprehenderit, doloribus, aliquam assumenda ut?
+            </p>
+            <Footer />
+        </div>
+    );
 }
