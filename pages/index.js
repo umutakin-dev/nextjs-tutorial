890e7417c89eafb95d7cf4dce5f057a243961977
Umut Akin <umutakin@fastmail.com>
2021-04-20 21:01:57 +0300

    nextjs 05 - creating a layout component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import Head from "next/head";
 import Link from "next/link";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
     return (
         <div>
-            <Navbar />
             <h1>Home Page</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
@@ -24,7 +21,6 @@ export default function Home() {
             <Link href="/ninjas">
                 <a>See Ninja Listing</a>
             </Link>s
-            <Footer />
         </div>
     );
 }
