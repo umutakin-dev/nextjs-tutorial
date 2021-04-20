614190b0ee08771662d9c7de87497dbf5fd850e1
Umut Akin <umutakin@fastmail.com>
2021-04-20 22:21:39 +0300

    nextjs 12 - dynamic routes (part 2 - getstaticpaths)

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,9 +1,26 @@
+export const getStaticPaths = async () => {
+    const uri = "https://jsonplaceholder.typicode.com/users";
+    const res = await fetch(uri);
+    const data = await res.json();
+
+    const paths = data.map((ninja) => {
+        return {
+            params: { id: ninja.id.toString() },
+        };
+    });
+
+    return {
+        paths,
+        fallback: false,
+    };
+};
+
 const Details = () => {
     return (
         <div>
             <h1>Details Page</h1>
         </div>
     );
-}
- 
+};
+
 export default Details;
