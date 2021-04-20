948ac4f4633c39b5afd9f6562f0ced568b523cd2
Umut Akin <umutakin@fastmail.com>
2021-04-20 22:05:47 +0300

    nextjs 10 - fetching data (getstaticprops)

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,30 @@
-const Ninjas = () => {
+import styles from '../../styles/Ninjas.module.css';
+
+export const getStaticProps = async () => {
+    const uri = "https://jsonplaceholder.typicode.com/users";
+    const res = await fetch(uri);
+
+    const data = await res.json();
+
+    return {
+        props: { ninjas: data },
+    };
+};
+
+//const Ninjas = (props) => {
+const Ninjas = ({ ninjas }) => {
     return (
         <div>
             <h1>All Ninjas</h1>
+            {ninjas.map((ninja) => (
+                <div key={ninja.id}>
+                    <a className={styles.single}>
+                        <h3>{ninja.name}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
     );
-}
- 
+};
+
 export default Ninjas;
